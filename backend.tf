209bda1a759e30dcd5cddca573f41b3d36073bfc
Bridgetching <kongchingbridget@gmail.com>
2025-09-28 11:59:00 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mir-terraform-s3-bucket"
+    bucket = "b-infrastructure"
     key    = "key/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
